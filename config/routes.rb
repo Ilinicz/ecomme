3b9e16d2c96bbd4a276815d33cde054f9a9d10a5
Vincent Ilinicz <vilinicz@mail.ru>
2014-04-12 01:46:05 +0400

    re-routing login/join

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Ecomme::Application.routes.draw do
   root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  devise_for :users, :controllers => {:registrations => "registrations"}, 
+  :path => '', :path_names => {
+    :sign_in => 'login', 
+    :sign_out => 'logout', 
+    :sign_up => 'join', 
+    :edit => 'account/settings'
+  }
+resources :users
+
+
 end
