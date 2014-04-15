ab3b2be9d5282d9d4e6abcbcd5ce83a4319603cc
Vincent Ilinicz <vilinicz@mail.ru>
2014-04-15 14:50:32 +0400

    DB:SEED debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-
 Product.delete_all
 20.times do
     Product.create!(
